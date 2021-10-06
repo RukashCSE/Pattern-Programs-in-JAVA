2f8181afdc77a23fb00678348f882de1bd6a735d
Rukash <90242818+RukashCSE@users.noreply.github.com>
2021-10-06 07:11:02 +0530

    Reverse a num  4 3 2 1
    1
    21
    321
    4321

---
+++ Reversenum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Reversenum
+{
+    public static void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        { int m=i;
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(m+" ");
+                m--;
+            }
+            System.out.println("");
+        }
+    }
+}
+
