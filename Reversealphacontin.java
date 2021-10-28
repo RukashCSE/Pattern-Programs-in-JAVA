7a360321c69e1dcc9cd9d724dadc20c2d1a0dc73
Rukash <90242818+RukashCSE@users.noreply.github.com>
2021-10-28 14:11:13 +0530

    Reverse Alphabets Continuely from Z-A order

---
+++ Reversealphacontin.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Reversealphacontin 
+{
+    public static void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        int i,j;
+        char m='A';
+        
+        for(i=1;i<=n;i++)  //I want the exact number to reverse a pattern, run a dummy pattern to find the last num in patter.
+        { 
+            for(j=1;j<=i;j++)
+            { 
+                System.out.print(m+" ");
+                ++m;
+            }
+            System.out.println("");
+        }
+        m--;
+        char r=m;      
+         System.out.println("The Total numbers in pattern");      
+        System.out.println(r);
+        System.out.println("The value of 1st pattern "+m);
+         System.out.println("The REVERSE continue Alphabets:");
+        for(i=1;i<=n;i++)   //process of reverse
+        { 
+            for(j=1;j<=i;j++)
+            { 
+                System.out.print(r+" ");
+                r--;
+            }
+            System.out.println("");
+        }
+        
+        
+    }
+}
+
+
+
